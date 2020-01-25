bda3c4c05651fbd770a0fc824ff05c01ab2ca6b0
Avishek Das <avishek.akd@gmail.com>
2020-01-26 01:40:10 +0530

    g

--- test-dags/pythonOperator.py
+++ test-dags/pythonOperator.py
@@ -32,15 +32,10 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 
-# [END import_module]
-
-# [START default_args]
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
 default_args = {
-    'owner': 'airflow',
+    'owner': 'Airflow',
     'depends_on_past': False,
-    'start_date': days_ago(2),
+    'start_date': datetime(2015, 6, 1),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -50,74 +45,35 @@ default_args = {
     # 'pool': 'backfill',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
-    # 'on_retry_callback': another_function,
-    # 'sla_miss_callback': yet_another_function,
-    # 'trigger_rule': 'all_success'
 }
-# [END default_args]
 
-# [START instantiate_dag]
-dag = DAG(
-    'tutorial',
-    default_args=default_args,
-    description='A simple tutorial DAG',
-    schedule_interval=timedelta(days=1),
-    tags=['example'],
-)
-# [END instantiate_dag]
+dag = DAG('tutorial', default_args=default_args, schedule_interval=timedelta(days=1))
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
-# [START basic_task]
 t1 = BashOperator(
     task_id='print_date',
     bash_command='date',
-    dag=dag,
-)
+    dag=dag)
 
 t2 = BashOperator(
     task_id='sleep',
-    depends_on_past=False,
     bash_command='sleep 5',
     retries=3,
-    dag=dag,
-)
-# [END basic_task]
+    dag=dag)
 
-# [START documentation]
-dag.doc_md = __doc__
-
-t1.doc_md = """\
-#### Task Documentation
-You can document your task using the attributes `doc_md` (markdown),
-`doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
-rendered in the UI's Task Instance Details page.
-![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
-"""
-# [END documentation]
-
-# [START jinja_template]
 templated_command = """
-{% for i in range(5) %}
-    echo "{{ ds }}"
-    echo "{{ macros.ds_add(ds, 7)}}"
-    echo "{{ params.my_param }}"
-{% endfor %}
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{ macros.ds_add(ds, 7)}}"
+        echo "{{ params.my_param }}"
+    {% endfor %}
 """
 
 t3 = BashOperator(
     task_id='templated',
-    depends_on_past=False,
     bash_command=templated_command,
     params={'my_param': 'Parameter I passed in'},
-    dag=dag,
-)
-# [END jinja_template]
+    dag=dag)
 
-t1 >> [t2, t3]
-# [END tutorial]
+t2.set_upstream(t1)
+t3.set_upstream(t1)
