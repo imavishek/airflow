1d8060d682766458940151e65283af6083a53b8b
Avishek Das <avishek.akd@gmail.com>
2020-01-27 21:48:00 +0530

    g

--- test-dags/airflow-demo.py
+++ test-dags/airflow-demo.py
@@ -7,18 +7,6 @@ from airflow.contrib.kubernetes.volume_mount import VolumeMount
 
 YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 
-volume_mount = VolumeMount('test-volume',
-                            mount_path='/root/mount_file',
-                            sub_path=None,
-                            read_only=True)
-volume_config= {
-    'persistentVolumeClaim':
-      {
-        'claimName': 'test-volume'
-      }
-    }
-volume = Volume(name='test-volume', configs=volume_config)
-
 with models.DAG(
         dag_id='airflow-demo',
         schedule_interval=datetime.timedelta(days=1),
@@ -30,8 +18,6 @@ with models.DAG(
         namespace='default',
         image='eu.gcr.io/taiyo-239217/dag:fae4887',
         arguments=["AlphaVantage()"],
-		volume=[],
-        volume_mounts=[],
         in_cluster=True,
         xcom_push=True,
         is_delete_operator_pod=True
